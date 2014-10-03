0d76fa9c679185cab05594c09149498afb1736bb
Nathan Longnecker <nathanlongnecker@gmail.com>
2014-10-03 00:45:11 -0400

    Add implementation of Row, Word, and WordType classes with tests.

--- src/model/AbstractWord.java
+++ src/model/AbstractWord.java
@@ -1,6 +1,13 @@
 package model;
 
 public abstract class AbstractWord {
-	
 
+    long id;
+
+    /**
+     * getValue returns the value of the AbstractWord as a String
+     *
+     * @return String The value of the AbstractWord
+     */
+    public abstract String getValue();
 }

35e95652345b7e67142bee1c8140ce047d0bbd0b
YumouWang <yumouwang@gmail.com>
2014-10-06 11:21:22 -0400

    change AreaView to a SplitPanel, Protect word can remove word but cannot add word yet...

--- src/views/AreaView.java
+++ src/views/AreaView.java
@@ -7,26 +7,19 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
 import models.GameState;
+import models.Position;
 
 import java.awt.Color;
 
 public class AreaView extends JFrame {
 
-	private JPanel contentPane;
+	JSplitPane jsp;  
+	JPanel p1,p2,p3;   
+	JButton bt; 
 	UnprotectedArea upa;
 	ProtectedArea pa;
 
-	/**
-	 * Create the frame.
-	 */
-	public AreaView(GameState gameState) {
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		
+	public AreaView(GameState gameState)  { 
 		
 		upa = new UnprotectedArea(this, gameState);
 		upa.Unprotect(gameState);
@@ -34,35 +27,41 @@ public class AreaView extends JFrame {
 		
 		pa = new ProtectedArea(gameState);
 		pa.setBorder(new LineBorder(new Color(0, 0, 0)));
-
-		contentPane.setLayout(null);
 		
-		upa.setBounds(0, 141, 433, 121);
-		contentPane.add(upa);
-		upa.setLayout(null);
-		
-		pa.setBounds(0, 0, 433, 141);
-		contentPane.add(pa);
-		pa.setLayout(null);
-		
+		p1=new JPanel();   
+		p2=new JPanel();   
+		p1 = pa;
+		p2 = upa;
+		       
+		jsp=new JSplitPane(JSplitPane.VERTICAL_SPLIT,p1,p2);         
+		jsp.setDividerLocation(150);   
+		add(jsp,BorderLayout.CENTER);     
+		setDefaultCloseOperation(EXIT_ON_CLOSE);  
+		setSize(400,300);   
+		setResizable(false);   
+		//setVisible(true);    
+		setTitle("JSplitPane Demo");      
 	}
-	public void addAbstractWordView(AbstractWordView newWord) {
-        //words.add(newWord);
-        contentPane.add(newWord.label);
-        
-    }
 	
-    public void removeAbstractWordView(AbstractWordView oldWord) {
+	public void removeAbstractWordView(AbstractWordView oldWord) {
         //contentPane.remove(oldWord.label);
     	System.out.println(oldWord.getWord().getValue() + "---");
-    	contentPane.remove(upa.panel);
-    	contentPane.updateUI();
-    	upa.panel.remove(oldWord.label);
-    	oldWord.label.setVisible(false);
+    	upa.remove(oldWord.label);
+    	addAbstractWordView(oldWord);
+    	//oldWord.label.setVisible(false);
     	//contentPane
-    	pa.updateUI();
-    	contentPane.add(upa);
-    	contentPane.updateUI();
+    	jsp.updateUI();
+        	
+	}
+  
+	public void addAbstractWordView(AbstractWordView newWord) {
+        int x = 0;
+    	int y = 0;
+    	AbstractWordView view = new AbstractWordView(newWord.getWord(), new Position(x, y));
+    	//words.add(view);
+    	pa.panel.add(view.label);
+    	p1.updateUI();
+        
     }
 
 }
